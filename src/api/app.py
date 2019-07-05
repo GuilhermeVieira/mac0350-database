3f745a711c89476d744689554a55d13aca2ae477
GuilhermeVieira <xgvieira@gmail.com>
2019-07-05 19:18:10 -0300

    Add simple login form

--- src/api/app.py
+++ src/api/app.py
@@ -1,12 +1,12 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin
 
 
 projectName = "GradUSP"
 
-app = Flask(projectName)
+app = Flask(projectName, template_folder = '.')
 
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -14,7 +14,10 @@ accessdb = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
+'''
 class User(UserMixin, accessdb.model):
+'''
+
 
 @app.route('/')
 def login():
