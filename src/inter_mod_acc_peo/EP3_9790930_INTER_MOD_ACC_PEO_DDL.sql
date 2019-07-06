acb7d1b0d9bdfd7be1d7b87a9774342bf94720c9
ikedawalk <arukimi@gmail.com>
2019-07-06 17:16:55 -0300

    Scripts SQL ACC_PEO

--- src/inter_mod_acc_peo/EP3_9790930_INTER_MOD_ACC_PEO_DDL.sql
+++ src/inter_mod_acc_peo/EP3_9790930_INTER_MOD_ACC_PEO_DDL.sql
@@ -17,3 +17,10 @@ GRANT USAGE ON SCHEMA admins TO dba;
 -- DROP DOMAIN IF EXISTS email CASCADE;
 CREATE DOMAIN email AS citext
   CHECK ( value ~ '^[a-zA-Z0-9.!#$%&''*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$' );
+
+CREATE TABLE b24_REL_US_PE (
+  us_email        email,
+  pe_nusp         int NOT NULL check (pe_nusp between 0 and 999999999),
+  CONSTRAINT pk_rel_us_pe PRIMARY KEY (pe_nusp),
+  CONSTRAINT sk_rel_us_pe UNIQUE (us_email)
+);
--- src/inter_mod_acc_peo/EP3_9790930_INTER_MOD_ACC_PEO_FUNCTIONS.sql
+++ src/inter_mod_acc_peo/EP3_9790930_INTER_MOD_ACC_PEO_FUNCTIONS.sql
@@ -1,3 +1,56 @@
 \c acc_peo;
 
--- obs: criar uma função que associe pessoa com user (em função da remoção do atributo user_id de pessoa)
+--------------------------------------------------------------------------------
+-- Create Group
+--------------------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION relaciona_usuario_pessoa (IN us_email email, INOUT pe_nusp int)
+  RETURNS int AS
+  $$
+  INSERT INTO b24_REL_US_PE(us_email, pe_nusp) VALUES ($1, $2)
+  RETURNING pe_nusp
+  $$
+  LANGUAGE sql
+  SECURITY DEFINER
+  SET search_path FROM CURRENT;
+
+--------------------------------------------------------------------------------
+-- Update Group
+--------------------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION atualiza_rel_us_pe_email (INOUT us_email email, INOUT pe_nusp int)
+  RETURNS SETOF RECORD AS
+  $$
+  UPDATE b24_REL_US_PE SET us_email = $1 WHERE pe_nusp = $2
+  RETURNING us_email, pe_nusp
+  $$
+  LANGUAGE sql
+  SECURITY DEFINER
+  SET search_path FROM CURRENT;
+
+--------------------------------------------------------------------------------
+-- Delete Group
+--------------------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION remove_rel_us_pe (INOUT pe_nusp int)
+  RETURNS int AS
+  $$
+  DELETE FROM b24_REL_US_PE WHERE pe_nusp = $1
+  RETURNING pe_nusp
+  $$
+  LANGUAGE sql
+  SECURITY DEFINER
+  SET search_path FROM CURRENT;
+
+--------------------------------------------------------------------------------
+-- Retrieval Group
+--------------------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION verifica_email_existe (IN us_email email)
+  RETURNS boolean AS
+  $$
+  SELECT EXISTS(SELECT * FROM b24_REL_US_PE WHERE us_email = $1)
+  $$
+  LANGUAGE sql
+  SECURITY DEFINER
+  SET search_path FROM CURRENT;
