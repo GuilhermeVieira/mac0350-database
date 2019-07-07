7477a66ed3776466dc177339b6a772ed0c9d3962
ikedawalk <arukimi@gmail.com>
2019-07-06 21:53:33 -0300

    Progresso scripts SQL

--- src/inter_mod_peo_cur/EP3_9790930_INTER_MOD_PEO_CUR_FUNCTIONS.sql
+++ src/inter_mod_peo_cur/EP3_9790930_INTER_MOD_PEO_CUR_FUNCTIONS.sql
@@ -3,25 +3,154 @@
 -- criar função de associação entre oferecimento e professor (remoção do atributo pf_cpf de oferecimento)
 
 --------------------------------------------------------------------------------
+-- Create Group
+--------------------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION inicia_curso(al_pe_nusp INT, cur_codigo INT, data_ingresso INT) RETURNS VOID
+    AS $$ BEGIN
+        INSERT INTO b15_REL_AL_CUR (al_pe_nusp, cur_codigo, data_ingresso, status) VALUES ($1, $2, $3, 'A');
+    END; $$
+    LANGUAGE plpgsql
+    SECURITY DEFINER
+    SET search_path FROM CURRENT;
+
+CREATE OR REPLACE FUNCTION planeja_disciplina(al_nusp INT, dis_data_inicio CHAR(4), dis_departamento CHAR(3), dis_codigo CHAR(4), pl_ano CHAR(4), pl_semestre INT) RETURNS VOID
+    AS $$ BEGIN
+        INSERT INTO b19_PLANEJA (al_pe_nusp, dis_data_inicio, dis_departamento, dis_codigo, ano, semestre) VALUES ($1, $2, $3, $4, $5, $6);
+    END; $$
+    LANGUAGE plpgsql
+    SECURITY DEFINER
+    SET search_path FROM CURRENT;
+
+CREATE OR REPLACE FUNCTION inicia_ministracao(pf_pe_nusp INT, dis_data_inicio CHAR(4), dis_departamento CHAR(3), dis_codigo CHAR(4)) RETURNS VOID
+    AS $$ BEGIN
+        INSERT INTO b20_MINISTRA (pf_pe_nusp, dis_data_inicio, dis_departamento, dis_codigo) VALUES ($1, $2, $3, $4);
+    END; $$
+    LANGUAGE plpgsql
+    SECURITY DEFINER
+    SET search_path FROM CURRENT;
+
+CREATE OR REPLACE FUNCTION inicia_administracao(ad_pe_nusp INT, cur_codigo INT, inicio_gestao CHAR(4)) RETURNS VOID
+    AS $$ BEGIN
+        INSERT INTO b21_ADMINISTRA (ad_pe_nusp, cur_codigo, inicio_gestao, fim_gestao) VALUES ($1, $2, $3, NULL);
+    END; $$
+    LANGUAGE plpgsql
+    SECURITY DEFINER
+    SET search_path FROM CURRENT;
+
+CREATE OR REPLACE FUNCTION oferece_ministracao(pe_nusp INT, dis_data_inicio CHAR(4), dis_departamento CHAR(3), dis_codigo CHAR(4), semestre INT, ano INT) RETURNS VOID
+    AS $$ BEGIN
+        INSERT INTO b22_OFERECIMENTO (pf_pe_nusp, dis_data_inicio, dis_departamento, dis_codigo, semestre, ano) VALUES ($1, $2, $3, $4, $5, $6);
+    END; $$
+    LANGUAGE plpgsql
+    SECURITY DEFINER
+    SET search_path FROM CURRENT;
+
+CREATE OR REPLACE FUNCTION cursa_disciplina(al_nusp INT, ofer_id INT, nota REAL, status CHAR(1)) RETURNS VOID
+    AS $$ BEGIN
+        INSERT INTO b23_CURSA (al_pe_nusp, of_id, nota, status) VALUES ($1, $2, $3, $4);
+    END; $$
+    LANGUAGE plpgsql
+    SECURITY DEFINER
+    SET search_path FROM CURRENT;
+
+CREATE OR REPLACE FUNCTION relaciona_ofer_prof(INOUT prof_nusp int, INOUT ofer_id SERIAL)
+    RETURNS SETOF RECORD AS
+    $$
+        INSERT INTO b25_REL_OFER_PROF(prof_nusp, ofer_id) VALUES ($1, $2)
+    $$
+    LANGUAGE sql
+    SECURITY DEFINER
+    SET search_path FROM CURRENT;
+
+--------------------------------------------------------------------------------
 -- Update Group
 --------------------------------------------------------------------------------
 
 CREATE OR REPLACE FUNCTION muda_status_aluno_no_curso(al_pe_nusp INT, cur_codigo INT, data_ingresso INT, status CHAR(1)) RETURNS VOID
-AS $$ BEGIN
+    AS $$ BEGIN
         UPDATE b15_REL_AL_CUR SET status=$4 WHERE b15_REL_AL_CUR.al_pe_nusp=$1 AND b15_REL_AL_CUR.cur_codigo=$2 AND b15_REL_AL_CUR.data_ingresso=$3;
-      END; $$
-LANGUAGE plpgsql
-SECURITY DEFINER
-SET search_path FROM CURRENT;
+    END; $$
+    LANGUAGE plpgsql
+    SECURITY DEFINER
+    SET search_path FROM CURRENT;
+
+CREATE OR REPLACE FUNCTION atualiza_encerramento_administracao(ad_pe_nusp INT, cur_codigo INT, inicio_gestao CHAR(4), fim_gestao CHAR(4)) RETURNS VOID
+    AS $$ BEGIN
+        UPDATE b21_ADMINISTRA SET fim_gestao=$4 WHERE b21_ADMINISTRA.ad_pe_nusp=$1 AND b21_ADMINISTRA.cur_codigo=$2 AND b21_ADMINISTRA.inicio_gestao=$3;
+    END; $$
+    LANGUAGE plpgsql
+    SECURITY DEFINER
+    SET search_path FROM CURRENT;
+
+CREATE OR REPLACE FUNCTION atualiza_nota_disciplina(al_nusp INT, of_id INT, nota REAL) RETURNS VOID
+    AS $$ BEGIN
+        UPDATE b23_CURSA SET nota=$3 WHERE al_pe_nusp=$1 AND of_id=$2;
+    END; $$
+    LANGUAGE plpgsql
+    SECURITY DEFINER
+    SET search_path FROM CURRENT;
+
+CREATE OR REPLACE FUNCTION atualiza_status_disciplina(al_nusp INT, of_id INT, status CHAR(1)) RETURNS VOID
+    AS $$ BEGIN
+        UPDATE b23_CURSA SET status=$3 WHERE al_pe_nusp=$1 AND of_id=$2;
+    END; $$
+    LANGUAGE plpgsql
+    SECURITY DEFINER
+    SET search_path FROM CURRENT;
 
 --------------------------------------------------------------------------------
 -- Delete Group
 --------------------------------------------------------------------------------
 
-CREATE OR REPLACE FUNCTION remove_curso(al_pe_nusp INT, cur_codigo INT) RETURNS VOID
-AS $$ BEGIN
-        DELETE FROM b15_REL_AL_CUR WHERE b15_REL_AL_CUR.al_pe_nusp=$1 AND b15_REL_AL_CUR.cur_codigo=$2;
-      END; $$
-LANGUAGE plpgsql
-SECURITY DEFINER
-SET search_path FROM CURRENT;
+CREATE OR REPLACE FUNCTION remove_curso(al_pe_nusp INT, cur_codigo INT, data_ingresso INT) RETURNS VOID
+    AS $$ BEGIN
+        DELETE FROM b15_REL_AL_CUR WHERE b15_REL_AL_CUR.al_pe_nusp=$1 AND b15_REL_AL_CUR.cur_codigo=$2 AND b15_REL_AL_CUR.data_ingresso=$3;
+    END; $$
+    LANGUAGE plpgsql
+    SECURITY DEFINER
+    SET search_path FROM CURRENT;
+
+CREATE OR REPLACE FUNCTION remove_planejamento(al_nusp INT, dis_data_inicio CHAR(4), dis_departamento CHAR(3), dis_codigo CHAR(4)) RETURNS VOID
+    AS $$ BEGIN
+        DELETE FROM b19_PLANEJA WHERE al_pe_nusp=$1 AND b19_PLANEJA.dis_data_inicio=$2 AND b19_PLANEJA.dis_departamento=$3 AND b19_PLANEJA.dis_codigo=$4;
+    END; $$
+    LANGUAGE plpgsql
+    SECURITY DEFINER
+    SET search_path FROM CURRENT;
+
+CREATE OR REPLACE FUNCTION remove_ministracao(pf_pe_nusp INT, dis_data_inicio CHAR(4), dis_departamento CHAR(3), dis_codigo CHAR(4)) RETURNS VOID
+    AS $$ BEGIN
+        DELETE FROM b20_MINISTRA WHERE b20_MINISTRA.pf_pe_nusp=$1 AND b20_MINISTRA.dis_data_inicio=$2 AND b20_MINISTRA.dis_departamento=$3 AND b20_MINISTRA.dis_codigo=$4;
+    END; $$
+    LANGUAGE plpgsql
+    SECURITY DEFINER
+    SET search_path FROM CURRENT;
+
+CREATE OR REPLACE FUNCTION remove_administracao(ad_pe_nusp INT, cur_codigo INT, inicio_gestao CHAR(4)) RETURNS VOID
+    AS $$ BEGIN
+        DELETE FROM b21_ADMINISTRA WHERE b21_ADMINISTRA.ad_pe_nusp=$1 AND b21_ADMINISTRA.cur_codigo=$2 AND b21_ADMINISTRA.inicio_gestao=$3;
+    END; $$
+    LANGUAGE plpgsql
+    SECURITY DEFINER
+    SET search_path FROM CURRENT;
+
+CREATE OR REPLACE FUNCTION remove_cursa(al_nusp INT, ofer_id INT) RETURNS VOID
+  AS $$ BEGIN
+      DELETE FROM b23_CURSA WHERE al_pe_nusp=$1 AND of_id=$2;
+  END; $$
+  LANGUAGE plpgsql
+  SECURITY DEFINER
+  SET search_path FROM CURRENT;
+
+CREATE OR REPLACE FUNCTION remove_rel_ofer_prof(IN nusp INT, IN of_id INT) RETURNS VOID
+  AS $$ BEGIN
+      DELETE FROM b25_REL_OFER_PROF WHERE (prof_nusp, ofer_id) = ($1, $2);
+  END; $$
+  LANGUAGE plpgsql
+  SECURITY DEFINER
+  SET search_path FROM CURRENT;
+
+--------------------------------------------------------------------------------
+-- Retrieval Group
+--------------------------------------------------------------------------------
